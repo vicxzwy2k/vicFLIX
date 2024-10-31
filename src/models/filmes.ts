d16a88aded5e68ac015ffe1c470a4e22bd18e3f2
VITOR HUGO ZAFFANI PAIXAO <vitor.hzpaixao@senacsp.edu.br>
2024-10-31 17:15:43 -0300

    atualização

--- src/models/filmes.ts
+++ src/models/filmes.ts
@@ -6,3 +6,36 @@
   3. FILTRAR ITENS PELO NOME
  
  */
+
+
+  type FilmesType = 'terror' | 'acao' | 'animacao'
+
+  type Filmes = {
+    type: FilmesType
+    image: string,
+    name: string,
+    color: string
+  }
+
+const data: Filmes[] = [
+
+]
+
+export const Filmes = {
+
+
+
+
+  getAll:() =>{
+    return data
+  },
+  getFromType: (type: FilmesType): Filmes[] =>{
+    return data.filter(item =>{
+      
+    })
+  }
+}
+
+
+
+
